e17f30c9f68e7b8139d2836890e95c9dced1c76a
Razenerau <molokov.phil@gmail.com>
2025-02-16 13:49:53 -0500

    Added player controls

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,5 +2,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody;
 
+    private float _xSpeed;
+    private float _ySpeed;
+    public float speed;
+
+    private const string xInput = "Horizontal";
+    private const string yInput = "Vertical";
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _xSpeed = Input.GetAxis(xInput);
+        _ySpeed = Input.GetAxis(yInput);
+
+        _rigidbody.velocity = new Vector2(_xSpeed, _ySpeed) * speed;
+    }
 }
--- Assets/Scripts/PlayerRotation.cs
+++ Assets/Scripts/PlayerRotation.cs
@@ -2,5 +2,24 @@ using UnityEngine;
 
 public class PlayerRotation : MonoBehaviour
 {
+    private Camera _camera;
+    private const string _cameraName = "Game_Camera";
 
+    private Vector3 _mousePosition;
+
+    private void Start()
+    {
+        _camera = GameObject.Find(_cameraName).GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        // Gets mouse position relative to where the camera sees it
+        _mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+
+                   //Where the mouse is \/          Where the player is \/
+        Vector3 positionDifference = _mousePosition - transform.position;
+        float rotationZ = Mathf.Atan2(positionDifference.x, positionDifference.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotationZ - 90;
+    }
 }
