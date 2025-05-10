c0c7a0e6950b070d3348808bb986bbff7b5eab0e
Razenerau <molokov.phil@gmail.com>
2025-05-10 13:58:12 -0400

    tutorial

--- Assets/SpawnerManager.cs
+++ Assets/SpawnerManager.cs
@@ -8,13 +8,35 @@ public class SpawnerManager : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        StartCoroutine(LoadPrefabAfterDelay(3.5f));
+        StartCoroutine(LoadPrefabAfterDelay(1f));
     }
 
     private System.Collections.IEnumerator LoadPrefabAfterDelay(float delay)
     {
+        StartCounterView.SetVisible(false);
+
         yield return new WaitForSeconds(delay);
-        Debug.Log("5 seconds passed");
+        StartCounterView.SetVisible(true);
+        StartCounterView.SetText("3");
+
+        yield return new WaitForSeconds(delay);
+        StartCounterView.SetText("2");
+
+        yield return new WaitForSeconds(delay);
+        StartCounterView.SetText("1");
+
+        yield return new WaitForSeconds(delay);
+
+        for (int i = 0; i < 5; i++)
+        {
+            yield return new WaitForSeconds(delay / 5);
+            StartCounterView.SetText("GO GO GO");
+            StartCounterView.SetVisible(true);
+
+            yield return new WaitForSeconds(delay / 5);
+            StartCounterView.SetVisible(false);
+        }
+
         Instantiate(_preFab, Vector3.zero, Quaternion.identity);
     }
 
---
+++ Assets/StartCounterView.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class StartCounterView : MonoBehaviour
+{
+    public static StartCounterView Instance;
+    private static TextMeshProUGUI _text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+        _text.text = "3";
+    }
+
+    public static void SetText(string text)
+    {
+        _text.text = text;
+    }
+
+    public static void SetVisible(bool isvisible)
+    {
+        Color newColor;
+        switch (isvisible)
+        {
+            case true:
+                 newColor = new Color(_text.color.r, _text.color.g, _text.color.b, 1f);
+                _text.color = newColor;
+                break;
+
+            case false:
+                 newColor = new Color(_text.color.r, _text.color.g, _text.color.b, 0f);
+                _text.color = newColor;
+                break;
+        }
+        
+    }
+
+}
