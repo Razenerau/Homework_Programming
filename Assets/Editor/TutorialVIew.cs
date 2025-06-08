e5ed6719ab3b3b789ace30c052550f51172be0b1
Razenerau <molokov.phil@gmail.com>
2025-06-08 12:00:40 -0400

    smoothly lerped scissors into the screen

--- Assets/Editor/TutorialVIew.cs
+++ Assets/Editor/TutorialVIew.cs
@@ -38,27 +38,30 @@ public class TutorialVIew : MonoBehaviour
 
     public void ShootTutorial()
     {
-        Vector2 pos = new Vector3(8, 0, 0);
-        Vector2 targetPos = new Vector3(4, 0, 0);
+        Vector3 pos = new Vector3(12, 0, 0);
+        Vector3 targetPos = new Vector3(4, 0, 0);
         GameObject scissorsEnemy = Instantiate(_scissorsEnemy, pos, Quaternion.Euler(0, 0, 90), gameObject.transform);
-        StartCoroutine(MoveObject(scissorsEnemy, targetPos, 5));
+        
+        ScissorsEnemy script = scissorsEnemy.GetComponent<ScissorsEnemy>();
+        script.deathTime = 999999f;
+
+        StartCoroutine(MoveObject(scissorsEnemy, targetPos, 100));
     }
 
     private IEnumerator MoveObject(GameObject gameObject, Vector3 targetPos, float moveDuration)
     {
-        //float threshold = 0.01f;
-        /*while (Vector3.Distance(transform.position, targetPos) > threshold)
-        {
-            Vector3 pos = gameObject.transform.position;
-            pos = Vector3.MoveTowards(pos, targetPos, speed * Time.deltaTime);
-            yield return null; // Waits for the next frame
-        }*/
+        
         Vector3 startPos = gameObject.transform.position;
         float elapsedTime = 0;
         while (elapsedTime < moveDuration)
         {
-            transform.position = Vector3.Lerp(startPos, targetPos, elapsedTime / moveDuration);
-            elapsedTime += Time.deltaTime;
+            float startingX = gameObject.transform.position.x;
+            float finalX = targetPos.x;
+            float slope = (finalX - startingX) / moveDuration;
+            float currentX = slope * elapsedTime + startingX;
+
+            gameObject.transform.position = new Vector3(currentX, 0, 0);
+            elapsedTime += Time.deltaTime * 0.1f;
             yield return null;
         }
 
